8cc3bbde7ce1ac567d9bdf3c2f78927ffad8377f
morcego69 <eduardo.oliveira12@escola.pr.gov.br>
2026-06-01 08:52:24 -0300

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+const botoesProximo = document.querySelectorAll(".btn-proximo");
 
+botoesProximo.forEach(botao => {
+    botao.addEventListener("click", () => {
+        document.querySelector(".ativo").classList.remove("ativo");
+
+        const proximoPasso = botao.getAttribute("data-proximo");
+        document.getElementById(`passo-${proximoPasso}`).classList.add("ativo");
+    });
+});
+
+const botoesVoltar = document.querySelectorAll(".btn-voltar");
+
+botoesVoltar.forEach(botao => {
+    botao.addEventListener("click", () => {
+        document.querySelector(".ativo").classList.remove("ativo");
+
+        const passoAnterior = botao.getAttribute("data-voltar");
+        document.getElementById(`passo-${passoAnterior}`).classList.add("ativo");
+    });
+});
